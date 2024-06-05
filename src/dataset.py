3105d999940251b1bc0583e943009880f24c0e49
nickash2 <nickash015@gmail.com>
2024-06-05 20:18:13 +0200

    rolled back datasets w/ comments

--- src/dataset.py
+++ src/dataset.py
@@ -2,23 +2,17 @@
 from torch.utils.data import Dataset
 import torch
 
-
+# Can mention in the report that after detrending, 
+# the data is randomised and dont need to keep track of the timeseries 
 class TimeSeriesDataset(Dataset):
     def __init__(self, data, window_size, timeseries_indices):
         self.data = data
         self.window_size = window_size
-        self.timeseries_indices = timeseries_indices
-        self.current_timeseries = 0
 
     def __len__(self):
         return len(self.data) - self.window_size
 
     def __getitem__(self, idx):
-        if idx + self.window_size >= self.timeseries_indices[self.current_timeseries + 1]:
-            self.current_timeseries += 1
-            idx = self.timeseries_indices[self.current_timeseries]
         X = self.data[idx : idx + self.window_size]
         y = self.data[idx + self.window_size]
         return X.clone().detach(), y.clone().detach()
-
-# timeseries_indices = list(df.groupby('ID').first().index.values) + [len(df)]
