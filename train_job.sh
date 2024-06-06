8be7fd3bcc1d090fa9c24c82062d6d8fd557e558
Nick <nickash015@gmail.com>
2024-06-06 13:20:25 +0200

    Added more habrok results

--- train_job.sh
+++ train_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=12:00:00
-#SBATCH --nodes=2
-#SBATCH --mem=8000
-#SBATCH --ntasks-per-node=2
-#SBATCH --output=job-%j.log
+#SBATCH --time=4:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=16GB
+#SBATCH --ntasks-per-node=1
+#SBATCH --output=job-newblockedcv-%j.log
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=a100:2
 
