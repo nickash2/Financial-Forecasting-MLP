7c908691d7aee19713acf4ea1d042421cc9f77e2
nickash2 <nickash015@gmail.com>
2024-06-06 11:39:57 +0200

    changed blockedcv for experimentation

--- src/blockedcv.py
+++ src/blockedcv.py
@@ -3,18 +3,19 @@ import numpy as np
 
 class BlockedTimeSeriesSplit(BaseCrossValidator):
     def __init__(self, n_splits=5):
+        super().__init__()
         self.n_splits = n_splits
 
     def split(self, X, y=None, groups=None):
         n_samples = len(X)
-        fold_sizes = np.full(self.n_splits, n_samples // self.n_splits)
-        fold_sizes[:n_samples % self.n_splits] += 1
-        current = 0
-        for fold_size in fold_sizes:
-            start, stop = current, current + fold_size
-            if start != 0:  # Skip the first block for training set
-                yield np.arange(0, start), np.arange(start, stop)
-            current = stop
+        if self.n_splits > n_samples:
+            raise ValueError("Cannot have number of splits greater than the number of samples.")
+
+        fold_sizes = np.array_split(np.arange(n_samples), self.n_splits)
+        for i in range(1, self.n_splits):  # Skip the first block for training set
+            train_idx = np.concatenate(fold_sizes[:i])
+            val_idx = fold_sizes[i]
+            yield train_idx, val_idx
 
     def get_n_splits(self, X=None, y=None, groups=None):
         return self.n_splits
