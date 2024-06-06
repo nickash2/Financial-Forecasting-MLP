a60a2ed11249c41d76fe149684fb1742f81ff3ad
Nick <nickash015@gmail.com>
2024-06-06 18:16:40 +0200

    Added final tuning training

--- train_job.sh
+++ train_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --time=4:00:00
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --mem=16GB
 #SBATCH --ntasks-per-node=1
 #SBATCH --output=job-newblockedcv-%j.log
 #SBATCH --partition=gpu
-#SBATCH --gpus-per-node=a100:2
+#SBATCH --gpus-per-node=1
 
 module load PyTorch
 pip install -r requirements.txt
