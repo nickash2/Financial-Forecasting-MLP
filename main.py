1be361b3e86f889e23c141e9ac18112e974902fa
nickash2 <nickash015@gmail.com>
2024-06-10 14:27:49 +0200

    Refactored to train with sMAPE

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ if __name__ == "__main__":
     print("Device:", device)
     dataset = preprocess_data_and_create_dataset()
     train_val_data, test_data = split_data(dataset)
-    tuning_mode = False
+    tuning_mode = True
     if tuning_mode:
         study = create_study_and_pruner()
         study = tuning_mode_operation(dataset, train_val_data, study, device)
@@ -104,33 +104,33 @@ if __name__ == "__main__":
             study = pickle.load(f)
             plot_best(study)
 
-    # Do predictions
-    predictor = Predictor(model_path="models/final_model.pth", device=device)
+        # Do predictions
+        predictor = Predictor(model_path="models/final_model.pth", device=device)
 
-    test_windows = torch.stack([test_data[i][0] for i in range(len(test_data))])
+        test_windows = torch.stack([test_data[i][0] for i in range(len(test_data))])
 
-    predictions = []
-    for i in range(len(test_windows)):
-        window = test_windows[i].numpy()
-        next_prediction = predictor.predict_next(window)
-        predictions.append(next_prediction)
-        print(f"Window {i}: {window} -> Next predicted point: {next_prediction}")
+        predictions = []
+        for i in range(len(test_windows)):
+            window = test_windows[i].numpy()
+            next_prediction = predictor.predict_next(window)
+            predictions.append(next_prediction)
+            print(f"Window {i}: {window} -> Next predicted point: {next_prediction}")
 
-    predictions = np.array(predictions)
-    true_values = torch.tensor([test_data[i][1] for i in range(len(test_data))]).numpy()
+        predictions = np.array(predictions)
+        true_values = torch.tensor([test_data[i][1] for i in range(len(test_data))]).numpy()
 
 
-    # Plot predictions vs actual values
-    plt.figure(figsize=(10, 6))
-    plt.plot(true_values, label='Actual', color='blue')
-    plt.plot(predictions, label='Predicted', color='red')
-    plt.title('Test Data: Actual vs Predicted')
-    plt.xlabel('Time')
-    plt.ylabel('Value')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+        # Plot predictions vs actual values
+        plt.figure(figsize=(10, 6))
+        plt.plot(true_values, label='Actual', color='blue')
+        plt.plot(predictions, label='Predicted', color='red')
+        plt.title('Test Data: Actual vs Predicted')
+        plt.xlabel('Time')
+        plt.ylabel('Value')
+        plt.legend()
+        plt.grid(True)
+        plt.show()
 
-    # Calculate the SMAPE
-    smape_value = predictor.accuracy(true_values, predictions)
-    print(f"SMAPE: {smape_value:.2f}%")
+        # Calculate the SMAPE
+        smape_value = predictor.accuracy(true_values, predictions)
+        print(f"SMAPE: {smape_value:.2f}%")
