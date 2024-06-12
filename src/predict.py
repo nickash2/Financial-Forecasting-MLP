9a619689f5ec22ee76f7fad9e069d5f124a4b1e6
nickash2 <nickash015@gmail.com>
2024-06-12 14:45:37 +0200

    habrok

--- src/predict.py
+++ src/predict.py
@@ -4,6 +4,7 @@ from .mlp import SMAPELoss, MLP
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+
 class Predictor:
     def __init__(self, model_path, device):
         self.model = MLP()  # Initialize the model first
@@ -11,7 +12,9 @@ class Predictor:
             self.model.load_state_dict(torch.load(model_path))
             self.model.to(device)
         else:
-            self.model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+            self.model.load_state_dict(
+                torch.load(model_path, map_location=torch.device("cpu"))
+            )
         self.device = device
 
     def retrend_data(detrended_data, original_data):
@@ -46,7 +49,11 @@ class Predictor:
         self.model.eval()
         with torch.no_grad():
             # Convert the last window to a tensor, add a batch dimension, and move it to the device
-            last_window = torch.tensor(last_window, dtype=torch.float32).unsqueeze(0).to(self.device)
+            last_window = (
+                torch.tensor(last_window, dtype=torch.float32)
+                .unsqueeze(0)
+                .to(self.device)
+            )
             # Predict the next point
             prediction = self.model(last_window)
         return prediction.squeeze().cpu().numpy()
--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
 # This file contains the training loop for the MLP model
-from .mlp import MLP, SMAPELoss
+from .mlp import MLP
 import optuna
 import torch
 from tqdm import tqdm
@@ -36,19 +36,16 @@ def train_epoch(
     train_loader,
     criterion,
     optimizer,
-    # scheduler,  # Include scheduler
     device,
     val_loader,
     trial,
     epoch,
     num_epochs,
-    early_stopping_patience=10,
+    early_stopping_patience=10
 ):
     model = model.to(device)
     model.train()
     total_loss = 0
-    early_stopping_counter = 0
-    best_val_loss = float('inf')
 
     progress_bar = tqdm(
         train_loader, desc=f"Epoch {epoch+1}/{num_epochs}", position=0, leave=True
@@ -56,7 +53,9 @@ def train_epoch(
 
     for inputs, targets in progress_bar:
         inputs = inputs.to(device)
-        targets = targets.to(device).view(-1, 1)  # Reshape the targets tensor to match the output tensor
+        targets = targets.to(device).view(
+            -1, 1
+        )  # Reshape the targets tensor to match the output tensor
         optimizer.zero_grad()
         outputs = model(inputs)
         loss = criterion(outputs, targets)
@@ -67,75 +66,83 @@ def train_epoch(
 
     avg_loss = total_loss / len(train_loader)
     val_loss = evaluate(model, val_loader, criterion, device)
-    # scheduler.step(val_loss)  # Update learning rate based on validation loss
     progress_bar.set_postfix(
         {"training_loss": "{:.3f}".format(avg_loss), "val_loss": val_loss}
     )
-    
-    trial.report(val_loss, epoch)
-    if trial is not None:
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            early_stopping_counter = 0
-        else:
-            early_stopping_counter += 1
 
-        if early_stopping_counter >= early_stopping_patience:
-            print("Early stopping triggered")
-            return best_val_loss  # Report the best validation loss
+    trial.report(val_loss, epoch)
 
     if trial is not None and trial.should_prune():
         raise optuna.TrialPruned()
 
-    print(f"Epoch {epoch+1}/{num_epochs}, Average Loss: {avg_loss}, Validation Loss: {val_loss}")
+    print(
+        f"Epoch {epoch+1}/{num_epochs}, Average Loss: {avg_loss}, Validation Loss: {val_loss}"
+    )
+
     return avg_loss, val_loss
 
 
-def train_final_model(train_val_data, combined_train_val_loader, best_params, device):
+def train_final_model(train_loader, best_params, device):
+
     model = MLP(
         input_size=INPUT_SIZE,
         hidden_size=int(best_params["hidden_size"]),
         output_size=OUTPUT_SIZE,
+        num_layers=int(best_params["hidden_layers"])
     )
     optimizer = torch.optim.Adam(
         model.parameters(), lr=best_params["lr"], weight_decay=best_params["lambda_reg"]
     )
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=5, min_lr=1e-7)
+    
     criterion = torch.nn.L1Loss()
     model = model.to(device)
     criterion = criterion.to(device)
-    num_epochs = 50
+    num_epochs = best_params["num_epochs"]
 
     train_losses = []
-    val_losses = []
 
     for epoch in range(num_epochs):
-        train_loss, val_loss = train_epoch(
-            model,
-            combined_train_val_loader,
-            criterion,
-            optimizer,
-            scheduler,  # Include scheduler
-            device,
-            combined_train_val_loader,  # Use combined_train_val_loader for validation
-            None,  # trial is not needed in final training
-            epoch,
-            num_epochs=num_epochs, # type: ignore
+        model.train()
+        total_loss = 0
+        progress_bar = tqdm(
+            train_loader, desc=f"Epoch {epoch+1}/{num_epochs}", position=0, leave=True
         )
-        train_losses.append(train_loss)
-        val_losses.append(val_loss)
+
+        for inputs, targets in progress_bar:
+            inputs = inputs.to(device)
+            targets = targets.to(device).view(
+                -1, 1
+            )  # Reshape the targets tensor to match the output tensor
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+
+        avg_loss = total_loss / len(train_loader)
+
+        progress_bar.set_postfix(
+            {"training_loss": "{:.3f}".format(avg_loss)}
+        )
+
+        train_losses.append(avg_loss)
+
+        print(
+            f"Epoch {epoch+1}/{num_epochs}, Average Loss: {avg_loss}"
+        )
 
     torch.save(model.state_dict(), "models/final_model.pth")
 
-    # Plot training and validation losses
+    # Plot training losses
     plt.figure()
     plt.plot(train_losses, label="Training Loss")
-    plt.plot(val_losses, label="Validation Loss")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
-    plt.title("Training and Validation Losses")
+    plt.title("Training Losses")
     plt.legend()
-    plt.savefig("plots/final_training_validation_loss.png")
+    plt.savefig("plots/final_training_loss.png")
 
 
 def objective(trial, dataset, device, n_splits=5):
@@ -145,21 +152,35 @@ def objective(trial, dataset, device, n_splits=5):
     for train_indices, val_indices in blocked_split.split(dataset):
         # Define hyperparameters using trial.suggest_*
         learning_rate = trial.suggest_float("lr", 1e-7, 1e-1, log=True)
-        hidden_size = trial.suggest_categorical("hidden_size", [2**i for i in range(4, 7)])
-        lambda_reg = trial.suggest_float("lambda_reg", 1e-7, 1.0, log=True)  # Increased upper limit
+        hidden_size = trial.suggest_categorical(
+            "hidden_size", [2**i for i in range(4, 7)]
+        )
+        lambda_reg = trial.suggest_float(
+            "lambda_reg", 1e-7, 1.0, log=True
+        )  # Increased upper limit
         hidden_layers = trial.suggest_int("hidden_layers", 1, 7)
         num_epochs = trial.suggest_int("num_epochs", 50, 100, step=10)
 
         train_subset = Subset(dataset, train_indices.tolist())
         val_subset = Subset(dataset, val_indices.tolist())
-        train_loader = DataLoader(train_subset, batch_size=32, shuffle=False, drop_last=True)
-        val_loader = DataLoader(val_subset, batch_size=32, shuffle=False, drop_last=False)
+        train_loader = DataLoader(
+            train_subset, batch_size=32, shuffle=False, drop_last=True
+        )
+        val_loader = DataLoader(
+            val_subset, batch_size=32, shuffle=False, drop_last=False
+        )
 
         # Define model and optimizer with the hyperparameters
-        model = MLP(input_size=INPUT_SIZE, hidden_size=hidden_size, output_size=OUTPUT_SIZE, num_layers=hidden_layers).to(device)
-        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=lambda_reg)
-
-        # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=5, min_lr=1e-7)
+        model = MLP(
+            input_size=INPUT_SIZE,
+            hidden_size=hidden_size,
+            output_size=OUTPUT_SIZE,
+            num_layers=hidden_layers,
+        ).to(device)
+        
+        optimizer = torch.optim.Adam(
+            model.parameters(), lr=learning_rate, weight_decay=lambda_reg
+        )
 
         # Define loss function
         criterion = torch.nn.L1Loss()
@@ -168,7 +189,7 @@ def objective(trial, dataset, device, n_splits=5):
         train_losses = []
         val_losses_fold = []
         for epoch in range(num_epochs):
-            train_loss, val_loss = train_epoch(model, train_loader, criterion, optimizer,device, val_loader, trial, epoch=epoch, num_epochs=num_epochs) # type: ignore
+            train_loss, val_loss = train_epoch(model, train_loader, criterion, optimizer, device, val_loader, trial, epoch=epoch, num_epochs=num_epochs)  # type: ignore
             train_losses.append(train_loss)
             val_losses_fold.append(val_loss)
 
@@ -189,4 +210,3 @@ def objective(trial, dataset, device, n_splits=5):
     plt.savefig(f"plots/trials/trial_{trial.number}_training_validation_loss.png")
 
     return avg_val_loss
-
